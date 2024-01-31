309b805acbd1aad373cabd768c7037d7cafa3fe1
Fasika Aga <fasikaaga7@gmail.com>
2024-01-31 16:56:03 -0500

    movies list added

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Link } from "react-router-dom";
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/movies">Movies</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
